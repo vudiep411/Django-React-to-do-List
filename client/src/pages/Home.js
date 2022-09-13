b9c66d963362f45de2059f3c39a066c44b63acd3
root <root@DESKTOP-1B723G7.localdomain>
2022-09-13 00:09:25 -0700

    delete extras

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { Button, Grid, Paper, TextField, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import { Container } from '@mui/system'
 import SearchBar from '../components/SearchBar'
 import MediaQuery from 'react-responsive'
 import TaskList from '../components/TaskList'
-import AddCircleIcon from '@mui/icons-material/AddCircle';
 import AddTasks from '../components/AddTasks'
 import { useDispatch } from 'react-redux'
 import { getTasks } from '../actions/tasks'
@@ -14,20 +12,18 @@ import { getTasks } from '../actions/tasks'
 const Home = () => {
     const dispatch = useDispatch()
     const [searchText, setSearchText] = useState("")
-    const [body, setBody] = useState('')
 
     useEffect(() => {
       dispatch(getTasks())
-    },
-    [dispatch])
+    }, [dispatch])
 
-    const handleUpdate = async (id) => {
-      await axios.put(`http://localhost:8000/notes/${id}/`, {body: body})
-    }
+    // const handleUpdate = async (id) => {
+    //   await axios.put(`http://localhost:8000/notes/${id}/`, {body: body})
+    // }
   return (
       <Container maxWidth='sm' style={{marginTop: '20px'}}>
         <div style={{justifyContent: 'space-between', display: 'flex'}}>
-          <Typography variant='h3' color='white'><b>To-do list</b></Typography><br/>
+          <Typography variant='h3' color='white'><b>Your Notes</b></Typography><br/>
           <MediaQuery minWidth={750}>
             <AddTasks width='500px'/>
           </MediaQuery>
