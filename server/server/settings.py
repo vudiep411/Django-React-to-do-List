14e1677ff22abb96682e86cdc472863075143e18
Vu Diep <54611122+vudiep411@users.noreply.github.com>
2022-09-16 00:16:42 -0700

    s

--- server/server/settings.py
+++ server/server/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%q1^i$7mz0h_9!=yfya7k58=pf*)5i61e5ux*o_%x%x%1f_ed8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-notes-todo-app.herokuapp.com', 'http://localhost:8000/']
 
 
 # Application definition
@@ -132,6 +133,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
